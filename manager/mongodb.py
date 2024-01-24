f44c1f9a6f0b2f2d3434ee96e07944d8606bdfa6
Egor Nikitin <eganik5674@gmail.com>
2024-01-24 22:17:05 +0300

    Add mongodb manager

---
+++ manager/mongodb.py
@@ -0,0 +1,15 @@
+import pymongo
+from settings.basic import MongoEngineSettings
+
+
+class MongoManager:
+    def __init__(self, mongo_settings: MongoEngineSettings) -> None:
+        self._con = pymongo.MongoClient(mongo_settings.host)
+        self._db = self._con[mongo_settings.db_name]
+        self._collection = self._db[mongo_settings.collection_name]
+
+    def get_employers_id(self) -> list:
+        """
+        Метод возвращает список с id работодателей hh.ru
+        """
+        return list(self._collection.find())[0].get("employers_id")
