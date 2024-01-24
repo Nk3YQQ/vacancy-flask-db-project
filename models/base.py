ceda944509130cf427c5878e32f5d1f5c82ea9ad
Egor Nikitin <eganik5674@gmail.com>
2024-01-24 22:13:34 +0300

    Add models/base

---
+++ models/base.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import DeclarativeBase, mapped_column
+from sqlalchemy import ForeignKey, String
+from typing import Annotated
+
+intpk = Annotated[int, mapped_column(primary_key=True)]
+employer_id = Annotated[int, mapped_column(ForeignKey('employers.employer_id', ondelete="CASCADE"))]
+str_40 = Annotated[str, 40]
+str_80 = Annotated[str, 80]
+
+
+class Base(DeclarativeBase):
+    type_annotation_map = {
+        str_40: String(40),
+        str_80: String(80)
+    }
