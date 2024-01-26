df3a5f84b1bc75e0e65ea0b14c1e3fdda0f10130
Egor Nikitin <eganik5674@gmail.com>
2024-01-26 20:11:51 +0300

    Update dbmanager

--- dbmanager/__init__.py
+++ dbmanager/__init__.py
@@ -1,4 +1,4 @@
-from sqlalchemy import func
+from sqlalchemy import desc, func, nullslast
 
 from src.utils import handle_array
 
@@ -9,16 +9,31 @@ class DBManager:
         self._employer_model = employer_model
         self._vacancy_model = vacancy_model
 
+    def get_all_vacancies(self):
+        with self._session() as session:
+            query = (
+                session.query(
+                    self._vacancy_model.vacancy_name,
+                    self._employer_model.employer_name,
+                    self._vacancy_model.salary,
+                    self._vacancy_model.requirements,
+                )
+                .join(self._vacancy_model)
+                .order_by(nullslast(desc(self._vacancy_model.salary)))
+                .all()
+            )
+            return handle_array(query)
+
     def get_companies_and_vacancies_count(self):
         with self._session() as session:
             query = (
                 session.query(
-                    self._employer_model.name,
+                    self._employer_model.employer_name,
                     func.count(self._vacancy_model.vacancy_id),
                 )
                 .join(self._vacancy_model)
-                .group_by(self._employer_model.name)
-                .order_by(func.count(self._vacancy_model.vacancy_id).desc())
+                .group_by(self._employer_model.employer_name)
+                .order_by(nullslast(desc(func.count(self._vacancy_model.vacancy_id))))
                 .all()
             )
             return list(map(lambda x: {"name": x[0], "vacancies": x[1]}, query))
@@ -31,8 +46,15 @@ class DBManager:
         with self._session() as session:
             average_salary = self.get_avg_salary()
             high_salary_vacancies = (
-                session.query(self._vacancy_model)
+                session.query(
+                    self._vacancy_model.vacancy_name,
+                    self._employer_model.employer_name,
+                    self._vacancy_model.salary,
+                    self._vacancy_model.requirements,
+                )
+                .join(self._employer_model)
                 .filter(self._vacancy_model.salary > average_salary)
+                .order_by(nullslast(desc(self._vacancy_model.salary)))
                 .all()
             )
             return handle_array(high_salary_vacancies)
@@ -40,11 +62,18 @@ class DBManager:
     def get_vacancies_with_keyword(self, keyword):
         with self._session() as session:
             query = (
-                session.query(self._vacancy_model)
+                session.query(
+                    self._vacancy_model.vacancy_name,
+                    self._employer_model.employer_name,
+                    self._vacancy_model.salary,
+                    self._vacancy_model.requirements,
+                )
+                .join(self._employer_model)
                 .filter(
-                    self._vacancy_model.name.ilike(f"%{keyword}%")
+                    self._vacancy_model.vacancy_name.ilike(f"%{keyword}%")
                     | self._vacancy_model.requirements.ilike(f"%{keyword}%")
                 )
+                .order_by(nullslast(desc(self._vacancy_model.salary)))
                 .all()
             )
             return handle_array(query)
