05f1418aa90f8b0dcc3160ec36817a1ebb4c6c3a
Egor Nikitin <eganik5674@gmail.com>
2024-01-26 15:25:24 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,22 @@
-from repository import EmployerRepository, VacancyRepository
-from src.parser import Parser
-from src.utils import get_employers_id
-from settings.basic import sql_engine
+from sqlalchemy.orm import sessionmaker
+
+from dbmanager import DBManager
+from dbmanager.mongodb import MongoManager
 from models import Employer, Vacancy
+from repository import EmployerRepository, VacancyRepository
+from settings.basic import mongo_settings, sql_engine
+from src.parser import Parser
 
-employers_repo = EmployerRepository(sql_engine, Employer)
-vacancy_repo = VacancyRepository(sql_engine, Vacancy)
+session = sessionmaker(sql_engine)
 
-employers_id = get_employers_id()
+employers_repo = EmployerRepository(session, Employer)
+vacancy_repo = VacancyRepository(session, Vacancy)
+
+employers_id = MongoManager(mongo_settings).get_employers_id()
 
 parser = Parser(employers_id)
 
-parser.search_employers(employers_repo, Employer)
-parser.search_vacancies(vacancy_repo, Vacancy)
+# parser.search_employers(employers_repo, Employer)
+# parser.search_vacancies(vacancy_repo, Vacancy)
+
+db_manager = DBManager(session, Employer, Vacancy)
