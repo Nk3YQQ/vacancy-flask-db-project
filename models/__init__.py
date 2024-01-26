c0a2bf7e7b9e7c6115f5d4d14ed1f6e14a820f98
Egor Nikitin <eganik5674@gmail.com>
2024-01-26 12:58:01 +0300

    Update models

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,11 @@
-from models.base import Base, intpk, employer_id, str_40, str_80
+from models.base import Base, intpk, employer_id, str_80
 from sqlalchemy.orm import Mapped
 from typing import Optional
+from settings.basic import sql_engine
 
 
 class Employer(Base):
-    __tablename__ = 'employees'
+    __tablename__ = 'employers'
     employer_id: Mapped[intpk]
     name: Mapped[str_80]
 
@@ -13,7 +14,9 @@ class Vacancy(Base):
     __tablename__ = 'vacancies'
     vacancy_id: Mapped[intpk]
     employer_id: Mapped[employer_id]
-    name: Mapped[str_40]
+    name: Mapped[str]
     salary: Mapped[Optional[int]]
-    description: Mapped[Optional[str]]
     requirements: Mapped[Optional[str]]
+
+
+Base.metadata.create_all(sql_engine)
