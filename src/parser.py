9a288f8bb42d19f3eec1caefc9b2b63054382265
Egor Nikitin <eganik5674@gmail.com>
2024-01-26 12:59:44 +0300

    Update parser.py

--- src/parser.py
+++ src/parser.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from src.utils import make_salary
 
 
 class Parser:
@@ -9,28 +10,27 @@ class Parser:
         self._vacancies_urls = list(map(lambda x: f"https://api.hh.ru/vacancies?employer_id={x}&per_page=10",
                                         self.employers_id))
 
-    def search_employers(self):
-        employers_list: list = []
+    def search_employers(self, repository, employer):
         for employer_url in self._employers_urls:
             raw_employer = json.loads(requests.get(employer_url).content)
-            employers = {"id": raw_employer["id"], "name": raw_employer["name"]}
-            employers_list.append(employers)
-        return employers_list
+            new_employer = employer(employer_id=raw_employer["id"], name=raw_employer["name"])
+            if repository.find(new_employer, employer):
+                continue
+            else:
+                repository.save(new_employer)
 
-    def search_vacancies(self):
-        vacancies_list: list = []
+    def search_vacancies(self, repository, vacancy):
         for vacancy_url in self._vacancies_urls:
             raw_vacancies = json.loads(requests.get(vacancy_url).content).get('items')
-            vacancies = list(map(
-                lambda x: {
-                    "id": x.get('id'),
-                    "name": x.get('name'),
-                    'salary': x.get('salary'),
-                    'description': x.get('description'),
-                    'requirements': x['snippet']['requirement']
-                },
-                raw_vacancies
-            )
-            )
-            vacancies_list.extend(vacancies)
-        return vacancies_list
+            for raw_vacancy in raw_vacancies:
+                new_vacancy = vacancy(
+                    vacancy_id=raw_vacancy['id'],
+                    employer_id=raw_vacancy['employer']['id'],
+                    name=raw_vacancy['name'],
+                    salary=make_salary(raw_vacancy['salary']),
+                    requirements=raw_vacancy['snippet']['requirement']
+                )
+                if repository.find(new_vacancy, vacancy):
+                    continue
+                else:
+                    repository.save(new_vacancy)
