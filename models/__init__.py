e5fe7c408d57c3860a6adfe7c18faf1129b4f663
Egor Nikitin <eganik5674@gmail.com>
2024-01-26 20:12:30 +0300

    Update models

--- models/__init__.py
+++ models/__init__.py
@@ -9,14 +9,14 @@ from settings.basic import sql_engine
 class Employer(Base):
     __tablename__ = "employers"
     employer_id: Mapped[intpk]
-    name: Mapped[str_80]
+    employer_name: Mapped[str_80]
 
 
 class Vacancy(Base):
     __tablename__ = "vacancies"
     vacancy_id: Mapped[intpk]
     employer_id: Mapped[employer_id]
-    name: Mapped[str]
+    vacancy_name: Mapped[str]
     salary: Mapped[Optional[int]]
     requirements: Mapped[Optional[str]]
 
