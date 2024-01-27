84437c0d950c6c7766410932eb287211c1bdc2e9
Egor Nikitin <eganik5674@gmail.com>
2024-01-27 16:36:54 +0300

    Update base.py

--- models/base.py
+++ models/base.py
@@ -4,9 +4,7 @@ from sqlalchemy import ForeignKey, String
 from sqlalchemy.orm import DeclarativeBase, mapped_column
 
 intpk = Annotated[int, mapped_column(primary_key=True)]
-employer_id = Annotated[
-    int, mapped_column(ForeignKey("employers.employer_id", ondelete="CASCADE"))
-]
+employer_id = Annotated[int, mapped_column(ForeignKey("employers.employer_id", ondelete="CASCADE"))]
 str_80 = Annotated[str, 80]
 
 
