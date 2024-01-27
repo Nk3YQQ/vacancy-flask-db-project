72c138baa2cbf3857e76c5f5f49ecf22b2a4ad2e
Egor Nikitin <eganik5674@gmail.com>
2024-01-27 16:36:47 +0300

    Update repository

--- repository/__init__.py
+++ repository/__init__.py
@@ -1,63 +1,72 @@
 from abc import ABC, abstractmethod
+from typing import Any
 
 
 class Repository(ABC):
     @abstractmethod
-    def save(self, entity):
+    def save(self, entity) -> None:
         pass
 
     @abstractmethod
-    def content(self):
+    def content(self) -> None:
         pass
 
     @abstractmethod
-    def find(self, entity, model):
+    def find(self, entity) -> None:
+        pass
+
+    @abstractmethod
+    def delete(self, entity) -> None:
         pass
 
 
 class EmployerRepository(Repository):
-    def __init__(self, session, model):
+    def __init__(self, session, model) -> None:
         self._session = session
         self._model = model
 
-    def save(self, entity):
+    def save(self, entity) -> None:
         with self._session() as session:
             session.add(entity)
             session.commit()
 
-    def content(self):
+    def content(self) -> list[Any]:
         with self._session() as session:
             return session.query(self._model).all()
 
-    def find(self, entity, model):
+    def find(self, entity) -> bool:
         with self._session() as session:
-            potential_entity = (
-                session.query(model)
-                .filter(model.employer_id == entity.employer_id)
-                .first()
-            )
+            potential_entity = session.query(self._model).filter(self._model.employer_id == entity.employer_id).first()
             return True if potential_entity else False
+
+    def delete(self, entity) -> None:
+        with self._session() as session:
+            entity = session.query(self._model).filter(self._model.employer_id == entity.employer_id).first()
+            session.delete(entity)
+            session.commit()
 
 
 class VacancyRepository(Repository):
-    def __init__(self, session, model):
+    def __init__(self, session, model) -> None:
         self._session = session
         self._model = model
 
-    def save(self, entity):
+    def save(self, entity) -> None:
         with self._session() as session:
             session.add(entity)
             session.commit()
 
-    def content(self):
+    def content(self) -> list[Any]:
         with self._session() as session:
             return session.query(self._model).all()
 
-    def find(self, entity, model):
+    def find(self, entity) -> bool:
         with self._session() as session:
-            potential_entity = (
-                session.query(model)
-                .filter(model.vacancy_id == entity.vacancy_id)
-                .first()
-            )
+            potential_entity = session.query(self._model).filter(self._model.vacancy_id == entity.vacancy_id).first()
             return True if potential_entity else False
+
+    def delete(self, entity) -> None:
+        with self._session() as session:
+            entity = session.query(self._model).filter(self._model.employer_id == entity.employer_id).first()
+            session.delete(entity)
+            session.commit()
