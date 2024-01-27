760c361941c4008c6371c55a0015afc3c1549ba6
Egor Nikitin <eganik5674@gmail.com>
2024-01-27 16:37:09 +0300

    Update parser.py

--- src/parser.py
+++ src/parser.py
@@ -9,12 +9,10 @@ logger = logging.getLogger(__name__)
 
 
 class Parser:
-    def __init__(self, employers_id, mongo_manager):
+    def __init__(self, employers_id: list[int], mongo_manager) -> None:
         self.employers_id = employers_id
         self._mongo_manager = mongo_manager
-        self._employers_urls = list(
-            map(lambda x: f"https://api.hh.ru/employers/{x}", self.employers_id)
-        )
+        self._employers_urls = list(map(lambda x: f"https://api.hh.ru/employers/{x}", self.employers_id))
         self._vacancies_urls = list(
             map(
                 lambda x: f"https://api.hh.ru/vacancies?employer_id={x}&per_page=10",
@@ -22,32 +20,30 @@ class Parser:
             )
         )
 
-    def search_employers(self, repository, employer):
-        logger.info('Start parsing employers...')
+    def search_employers(self, repository, employer) -> None:
+        logger.info("Start parsing employers...")
         for employer_url in self._employers_urls:
             raw_employer = json.loads(requests.get(employer_url).content)
             try:
-                new_employer = employer(
-                    employer_id=raw_employer["id"], employer_name=raw_employer["name"]
-                )
+                new_employer = employer(employer_id=raw_employer["id"], employer_name=raw_employer["name"])
             except KeyError:
-                logger.error('Invalid id for employer.')
+                logger.error("Invalid id for employer.")
                 self._mongo_manager.delete_employer(int(employer_url[28:]))
                 logger.info("Invalid id was deleted by Mongo manager. Don't worry.")
             else:
-                if repository.find(new_employer, employer):
-                    logger.info('Employer in database. Skipping it...')
+                if repository.find(new_employer):
+                    logger.info("Employer in database. Skipping it...")
                     continue
                 else:
                     repository.save(new_employer)
-                    logger.info('Employer has been added.')
+                    logger.info("Employer has been added.")
 
-    def search_vacancies(self, repository, vacancy):
-        logger.info('Start parsing vacancies...')
+    def search_vacancies(self, repository, vacancy) -> None:
+        logger.info("Start parsing vacancies...")
         for vacancy_url in self._vacancies_urls:
             raw_vacancies = json.loads(requests.get(vacancy_url).content).get("items")
             if not raw_vacancies:
-                logger.warning('Vacancy is empty. Skipping it...')
+                logger.warning("Vacancy is empty. Skipping it...")
                 continue
             for raw_vacancy in raw_vacancies:
                 new_vacancy = vacancy(
@@ -57,9 +53,9 @@ class Parser:
                     salary=make_salary(raw_vacancy["salary"]),
                     requirements=raw_vacancy["snippet"]["requirement"],
                 )
-                if repository.find(new_vacancy, vacancy):
-                    logger.info('Vacancy in database. Skipping it...')
+                if repository.find(new_vacancy):
+                    logger.info("Vacancy in database. Skipping it...")
                     continue
                 else:
                     repository.save(new_vacancy)
-                    logger.info('Vacancy has been added.')
+                    logger.info("Vacancy has been added.")
